bed43fff7ab7772d0a97853774412beac55c6a87
osdev01 <osmandev01@gmail.com>
2026-09-13 19:21:13 +0330

    revert: restore working messenger worker export

--- examples/chat-sdk-messenger/src/worker.ts
+++ examples/chat-sdk-messenger/src/worker.ts
@@ -1,20 +1,3 @@
-import {
-  ConversationAgent as BaseConversationAgent,
-  ThinkMessengerStateAgent,
-  getIngressAgentName,
-  ChatIngressAgent
-} from "./index";
-
-export class ConversationAgent extends BaseConversationAgent {
-  includeMcpTools = false;
-
-  override getTools() {
-    return {
-      ...super.getTools(),
-      ...this.mcp.getAITools()
-    };
-  }
-}
-
-export { ThinkMessengerStateAgent, getIngressAgentName, ChatIngressAgent };
+export { ConversationAgent } from "./index";
+export { ThinkMessengerStateAgent, getIngressAgentName, ChatIngressAgent } from "./index";
 export { default } from "./index";
